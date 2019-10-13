efa17bc911ac55b8348e1d48e95a1e39ad491301
Angelina Y <linocha1320@gmail.com>
2019-10-13 15:10:20 +0300

    add-mapping

--- Products/Controllers/ProductsController.cs
+++ Products/Controllers/ProductsController.cs
@@ -1,27 +1,33 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Threading.Tasks;
 using Products.Domain.Models;
 using Products.Domain.Services;
+using Products.Resources;
 
 namespace Products.Controllers
 {
-    [Route("/[controller]")]
+    [Route("/api/[controller]")]
     public class ProductsController : ControllerBase
     {
         private readonly IProductService _productService;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, IMapper mapper)
         {
             _productService = productService;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Product>> GetAllAsync()
+        public async Task<IEnumerable<ProductResource>> GetAllAsync()
         {
             var products = await _productService.ListAsync();
-            return products;
+            // map return data
+            var resources = _mapper.Map<IEnumerable<Product>, IEnumerable<ProductResource>>(products);
+            return resources;
         }
     }
 }
--- Products/Startup.cs
+++ Products/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -41,6 +43,8 @@ namespace Products
 
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IProductService, ProductService>();
+
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
