bc19edb72eab53a91fe1847eee28f98b41756537
Angelina Y <linocha1320@gmail.com>
2019-11-05 17:20:15 +0300

    Products: edit Route

--- Products/Domain/Models/Product.cs
+++ Products/Domain/Models/Product.cs
@@ -1,9 +1,9 @@
 namespace Products.Domain.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Cost { get; set; }
-    }
-}
+ {
+     public class Product
+     {
+         public int Id { get; set; }
+         public string Name { get; set; }
+         public double Cost { get; set; }
+     }
+ }
